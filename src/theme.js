1ca0f1bd8065f6b0c58ac9dc9878c8670da36448
duyuitk16 <nguyenvuanhduy173@gmail.com>
2023-10-21 22:53:35 +0700

    Dark Light P1

--- src/theme.js
+++ src/theme.js
@@ -1,24 +1,23 @@
-import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
+import { teal, deepOrange, cyan, orange } from '@mui/material/colors'
 
 // Create a theme instance.
-const theme = createTheme({
-  palette: {
-    mode: 'light', // default is light
-    primary: {
-      main: red[500]
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: teal,
+        secondary: deepOrange
+      }
     },
-    secondary: {
-      main: '#19857b'
-    },
-    error: {
-      main: red.A400
-    },
-    text: {
-      secondary: red[500]
+    dark: {
+      palette: {
+        primary: cyan,
+        secondary: orange
+      }
     }
-
   }
+  // ...other properties
 })
 
 export default theme
