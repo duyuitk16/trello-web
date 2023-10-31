93ec4f6933deccee81fd199f72255cb322b04a05
duyuitk16 <nguyenvuanhduy173@gmail.com>
2023-10-31 18:22:08 +0700

    Build Basic Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
-import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-import HomeIcon from '@mui/icons-material/Home'
-import { pink } from '@mui/material/colors'
-import Typography from '@mui/material/Typography'
 import { useColorScheme } from '@mui/material/styles'
-
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
 import Select from '@mui/material/Select'
-
 import LightModeIcon from '@mui/icons-material/LightMode'
 import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
 import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
 
 function ModeSelect() {
   const { mode, setMode } = useColorScheme()
@@ -33,7 +26,7 @@ function ModeSelect() {
         onChange={handleChange}
       >
         <MenuItem value="light">
-          <div style={{display:'flex', alignContent:'center', gap:'8px'}}>
+          <div style={{ display:'flex', alignContent:'center', gap:'8px' }}>
             <LightModeIcon/> Light
           </div>
         </MenuItem>
@@ -60,41 +53,37 @@ function ModeSelect() {
   )
 }
 
-function ModeToggle() {
-  const { mode, setMode } = useColorScheme()
-  return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
-  )
-}
-
 function App() {
   return (
-    <>
-      <ModeSelect />
-      <hr />
-      <ModeToggle />
-      <hr />
-      <div>NguyenVuAnhDuy</div>
-      <Typography variant="body2" color="text.secondary">Test Typography</Typography>
-      <Button variant="text">Text</Button>
-      <Button variant="contained" color='success'>Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <AccessAlarmIcon/>
-      <ThreeDRotation/>
-      <HomeIcon />
-      <HomeIcon color="primary" />
-      <HomeIcon color="secondary" />
-      <HomeIcon color="success" />
-      <HomeIcon color="action" />
-      <HomeIcon color="disabled" />
-      <HomeIcon sx={{ color: pink[900] }} />
-    </>
+    <Container disableGutters maxWidth={false} sx={{ height:'100vh' }}>
+      <Box sx={{
+        backgroundColor:'primary.light',
+        height:(theme) => theme.trello.appBarHeight,
+        width:'100%',
+        display:'flex',
+        alignItems:'center'
+      }}>
+        <ModeSelect />
+      </Box>
+      <Box sx={{
+        backgroundColor:'primary.dark',
+        height:(theme) => theme.trello.boardBarHeight,
+        width:'100%',
+        display:'flex',
+        alignItems:'center'
+      }}>
+        Board Bar
+      </Box>
+      <Box sx={{
+        backgroundColor:'primary.main',
+        height: (theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
+        width:'100%',
+        display:'flex',
+        alignItems:'center'
+      }}>
+        Board Content
+      </Box>
+    </Container>
   )
 }
 
--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,10 @@ import { teal, deepOrange, cyan, orange } from '@mui/material/colors'
 
 // Create a theme instance.
 const theme = extendTheme({
+  trello: {
+    appBarHeight: '48px',
+    boardBarHeight: '58px'
+  },
   colorSchemes: {
     light: {
       palette: {
