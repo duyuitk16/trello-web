a294f515263d2d0df0060816ec19ac6f2d539576
duyuitk16 <nguyenvuanhduy173@gmail.com>
2023-11-01 02:59:30 +0700

    Relative Absolute Import

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,7 +1,7 @@
 import Container from '@mui/material/Container'
-import AppBar from '../../components/AppBar'
-import BoardBar from '../BoardBar'
-import BoardContent from '../BoardContent'
+import AppBar from '~/components/AppBar'
+import BoardBar from '~/pages/Boards/BoardBar'
+import BoardContent from '~/pages/Boards/BoardContent'
 
 function Board() {
   return (
