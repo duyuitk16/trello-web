225acc37b3989926ebcfda28578a503b9a5ba85b
duyuitk16 <nguyenvuanhduy173@gmail.com>
2023-12-23 23:06:04 +0700

    FIX BUILD_MODE dev and prod

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,8 @@
-export const API_ROOT = 'http://localhost:8017'
-// export const API_ROOT = 'https://trello-api-99xq.onrender.com'
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-99xq.onrender.com'
+}
+export const API_ROOT = apiRoot
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Cho phép vite sử dụng process.env, mặc định thì không mà phải dùng import.meta.env
+  define: {
+    'process.env': process.env
+  },
   plugins: [
     react(),
     svgr()
